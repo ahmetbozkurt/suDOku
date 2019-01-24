66d4c7f616901d6863ac73d5bfb3c00323250807
Ahmet Bozkurt <ahmbozk@gmail.com>
2019-01-24 10:45:09 +0300

    grid system

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -1,8 +1,77 @@
 <template>
-  <div>
-    <v-btn color="success">Success</v-btn>
-    <v-btn color="error">Error</v-btn>
-    <v-btn color="warning">Warning</v-btn>
-    <v-btn color="info">Info</v-btn>
-  </div>
+  <v-container grid-list-md text-xs-center>
+
+    <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+      <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+        <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+      <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+        <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+      <v-layout row wrap>
+      <v-flex v-for="i in 9" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+  </v-container>
 </template>
